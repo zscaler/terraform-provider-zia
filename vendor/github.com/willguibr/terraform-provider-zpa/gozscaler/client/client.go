2582b830e7cf0903c3e9973d7a3b4f05524cd751
William Guilherme <wguilherme@securitygeek.io>
2021-09-24 21:14:06 -0700

    Initial Commit

--- vendor/github.com/willguibr/terraform-provider-zpa/gozscaler/client/client.go
+++ vendor/github.com/willguibr/terraform-provider-zpa/gozscaler/client/client.go
@@ -3,8 +3,10 @@ package client
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -12,15 +14,8 @@ import (
 	"github.com/willguibr/terraform-provider-zpa/gozscaler"
 )
 
-type AuthToken struct {
-	TokenType   string `json:"token_type"`
-	AccessToken string `json:"access_token"`
-	//ExpiresIn   string `json:"expires_in"`
-}
-
 type Client struct {
-	Config    *gozscaler.Config
-	AuthToken *AuthToken
+	Config *gozscaler.Config
 }
 
 // NewClient returns a new client for the specified apiKey.
@@ -32,8 +27,23 @@ func NewClient(config *gozscaler.Config) (c *Client) {
 	return
 }
 
+func (client *Client) NewPrivateRequestDo(method, url string, options, body, v interface{}) (*http.Response, error) {
+	return client.newRequestDoCustom(method, url, true, options, body, v)
+}
+
 func (client *Client) NewRequestDo(method, url string, options, body, v interface{}) (*http.Response, error) {
-	if client.AuthToken == nil {
+	return client.newRequestDoCustom(method, url, false, options, body, v)
+}
+
+func (client *Client) newRequestDoCustom(method, url string, usePrivateAPI bool, options, body, v interface{}) (*http.Response, error) {
+	client.Config.Lock()
+	defer client.Config.Unlock()
+	if client.Config.AuthToken == nil || client.Config.AuthToken.AccessToken == "" {
+		if client.Config.ClientID == "" || client.Config.ClientSecret == "" {
+			log.Printf("[ERROR] No client credentials were provided. Please set %s, %s and %s enviroment variables.\n", gozscaler.ZPA_CLIENT_ID, gozscaler.ZPA_CLIENT_SECRET, gozscaler.ZPA_CUSTOMER_ID)
+			return nil, errors.New("no client credentials were provided")
+		}
+		log.Printf("[TRACE] Getting access token for %s=%s\n", gozscaler.ZPA_CLIENT_ID, client.Config.ClientID)
 		formData := []byte(fmt.Sprintf("client_id=%s&client_secret=%s",
 			client.Config.ClientID,
 			client.Config.ClientSecret,
@@ -41,33 +51,36 @@ func (client *Client) NewRequestDo(method, url string, options, body, v interfac
 
 		req, err := http.NewRequest("POST", client.Config.BaseURL.String()+"/signin", bytes.NewBuffer(formData))
 		if err != nil {
+			log.Printf("[ERROR] Failed to signin the user %s=%s, err: %v\n", gozscaler.ZPA_CLIENT_ID, client.Config.ClientID, err)
 			return nil, err
 		}
 
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-		resp, err := client.Config.HTTPClient.Do(req)
+		resp, err := client.Config.GetHTTPClient().Do(req)
 
 		if err != nil {
+			log.Printf("[ERROR] Failed to signin the user %s=%s, err: %v\n", gozscaler.ZPA_CLIENT_ID, client.Config.ClientID, err)
 			return nil, err
 		}
 		defer resp.Body.Close()
 		respBody, err := io.ReadAll(resp.Body)
 		if err != nil {
+			log.Printf("[ERROR] Failed to signin the user %s=%s, err: %v\n", gozscaler.ZPA_CLIENT_ID, client.Config.ClientID, err)
 			return nil, err
 		}
 
-		var a AuthToken
+		var a gozscaler.AuthToken
 		err = json.Unmarshal(respBody, &a)
 		if err != nil {
+			log.Printf("[ERROR] Failed to signin the user %s=%s, err: %v\n", gozscaler.ZPA_CLIENT_ID, client.Config.ClientID, err)
 			return nil, err
 		}
 
 		// we need keep auth token for future http request
-		client.AuthToken = &a
+		client.Config.AuthToken = &a
 	}
-
-	req, err := client.newRequest(method, url, options, body)
+	req, err := client.newRequest(method, url, usePrivateAPI, options, body)
 	if err != nil {
 		return nil, err
 	}
@@ -76,7 +89,11 @@ func (client *Client) NewRequestDo(method, url string, options, body, v interfac
 }
 
 // Generating the Http request
-func (client *Client) newRequest(method, urlPath string, options, body interface{}) (*http.Request, error) {
+func (client *Client) newRequest(method, urlPath string, usePrivateAPI bool, options, body interface{}) (*http.Request, error) {
+	if client.Config.AuthToken == nil || client.Config.AuthToken.AccessToken == "" {
+		log.Printf("[ERROR] Failed to signin the user %s=%s\n", gozscaler.ZPA_CLIENT_ID, client.Config.ClientID)
+		return nil, fmt.Errorf("failed to signin the user %s=%s", gozscaler.ZPA_CLIENT_ID, client.Config.ClientID)
+	}
 	var buf io.ReadWriter
 	if body != nil {
 		buf = new(bytes.Buffer)
@@ -88,14 +105,17 @@ func (client *Client) newRequest(method, urlPath string, options, body interface
 
 	// Join the path to the base-url
 	u := *client.Config.BaseURL
+	if usePrivateAPI {
+		u = *client.Config.PrivateAPIBaseURL
+	}
 	unescaped, err := url.PathUnescape(urlPath)
 	if err != nil {
 		return nil, err
 	}
 
 	// Set the encoded path data
-	u.RawPath = client.Config.BaseURL.Path + urlPath
-	u.Path = client.Config.BaseURL.Path + unescaped
+	u.RawPath = u.Path + urlPath
+	u.Path = u.Path + unescaped
 
 	// Set the query parameters
 	if options != nil {
@@ -118,14 +138,14 @@ func (client *Client) newRequest(method, urlPath string, options, body interface
 	// 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	// }
 
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", client.AuthToken.AccessToken))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", client.Config.AuthToken.AccessToken))
 	req.Header.Add("Content-Type", "application/json")
 	//req.Header.Add("Accept", "application/json")
 	return req, nil
 }
 
 func (client *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
-	resp, err := client.Config.HTTPClient.Do(req)
+	resp, err := client.Config.GetHTTPClient().Do(req)
 	if err != nil {
 		return nil, err
 	}
