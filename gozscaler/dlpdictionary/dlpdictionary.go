4691b9747c221d9302240b8cbcb68aee903d0bf2
William Guilherme <wguilherme@securitygeek.io>
2021-09-26 15:06:02 -0700

    Initial Commit

--- gozscaler/dlpdictionary/dlpdictionary.go
+++ gozscaler/dlpdictionary/dlpdictionary.go
@@ -1,12 +1,14 @@
 package dlpdictionaries
 
 import (
-	"fmt"
+	"errors"
+	"log"
 	"net/http"
 )
 
 const (
-	dlpDictionariesEndpoint = "/dlpDictionaries"
+	dlpDictionariesEndpoint     = "/dlpDictionaries"
+	dlpDictionariesLiteEndpoint = "/dlpDictionaries/lite"
 )
 
 type DlpDictionary struct {
@@ -32,39 +34,59 @@ type Patterns struct {
 	Pattern string `json:"pattern,omitempty"`
 }
 
-func (service *Service) Get(dlpDictId string) (*DlpDictionary, *http.Response, error) {
-	v := new(DlpDictionary)
-	relativeURL := fmt.Sprintf("%s/%s", dlpDictionariesEndpoint, dlpDictId)
-	resp, err := service.Client.NewRequestDo("GET", relativeURL, nil, nil, v)
-	if err != nil {
-		return nil, nil, err
-	}
-	return v, resp, nil
-}
-
-func (service *Service) Create(dlpDict DlpDictionary) (*DlpDictionary, *http.Response, error) {
-	v := new(DlpDictionary)
-	resp, err := service.Client.NewRequestDo("POST", dlpDictionariesEndpoint, nil, dlpDict, &v)
-	if err != nil {
-		return nil, nil, err
-	}
-	return v, resp, nil
-}
-
-func (service *Service) Update(dlpDictId string, dlp DlpDictionary) (*http.Response, error) {
-	path := fmt.Sprintf("%s/%s", dlpDictionariesEndpoint, dlpDictId)
-	resp, err := service.Client.NewRequestDo("PUT", path, nil, dlpDictId, nil)
+func (service *Service) GetDlpDictionary(dlpDictionariesID string) (*DlpDictionary, error) {
+	var dlpDictionary DlpDictionary
+	err := service.Client.Read(dlpDictionariesEndpoint+"/"+dlpDictionariesID, &dlpDictionary)
 	if err != nil {
 		return nil, err
 	}
-	return resp, err
+
+	log.Printf("Returning information on all custom and predefined DLP dictionaries from Get: %s", dlpDictionary.ID)
+	return &dlpDictionary, nil
 }
 
-func (service *Service) Delete(dlpDictId string) (*http.Response, error) {
-	path := fmt.Sprintf("%s/%s", dlpDictionariesEndpoint, dlpDictId)
-	resp, err := service.Client.NewRequestDo("DELETE", path, nil, nil, nil)
+func (service *Service) GetDlpDictionaryLite() (*DlpDictionary, error) {
+	var dlpDictionary DlpDictionary
+	err := service.Client.Read(dlpDictionariesLiteEndpoint, &dlpDictionary)
 	if err != nil {
 		return nil, err
 	}
-	return resp, err
+
+	log.Printf("Returning name and ID dictionary of all custom and predefined DLP dictionaries from Get: %s", dlpDictionary.ID)
+	return &dlpDictionary, nil
+}
+
+func (service *Service) CreateDlpDictionary(dlpDictionariesID *DlpDictionary) (*DlpDictionary, *http.Response, error) {
+	resp, err := service.Client.Create(dlpDictionariesEndpoint, *dlpDictionariesID)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	createdDlpDictionary, ok := resp.(*DlpDictionary)
+	if !ok {
+		return nil, nil, errors.New("Object returned from API was not a Dlp Dictionary Pointer")
+	}
+
+	log.Printf("Returning new custom DLP dictionary that uses Patterns and Phrases from Create: %s", createdDlpDictionary.ID)
+	return createdDlpDictionary, nil, nil
+}
+
+func (service *Service) UpdateDlpDictionary(dlpDictionariesID string, dlpDictionaries *DlpDictionary) (*DlpDictionary, *http.Response, error) {
+	resp, err := service.Client.Update(dlpDictionariesEndpoint+"/"+dlpDictionariesID, *dlpDictionaries)
+	if err != nil {
+		return nil, nil, err
+	}
+	updatedDlpDictionary, _ := resp.(*DlpDictionary)
+
+	log.Printf("Returning updates custom DLP dictionary that uses Patterns and Phrases from Update: %s", updatedDlpDictionary.ID)
+	return updatedDlpDictionary, nil, nil
+}
+
+func (service *Service) DeleteDlpDictionary(dlpDictionariesID string) (*http.Response, error) {
+	err := service.Client.Delete(dlpDictionariesEndpoint + "/" + dlpDictionariesID)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
