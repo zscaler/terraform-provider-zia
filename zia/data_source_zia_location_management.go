d9008a19390f9d8177cddcb60acd1bc35a957394
William Guilherme <wguilherme@securitygeek.io>
2021-09-27 00:38:44 -0700

    Initial Commit

--- zia/data_source_zia_location_management.go
+++ zia/data_source_zia_location_management.go
@@ -1,7 +1,10 @@
 package zia
 
 import (
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/willguibr/terraform-provider-zia/gozscaler/locationmanagement"
 )
 
 func dataSourceLocationManagement() *schema.Resource {
@@ -191,39 +194,56 @@ func dataSourceLocationManagement() *schema.Resource {
 func dataSourceLocationManagementRead(d *schema.ResourceData, m interface{}) error {
 	zClient := m.(*Client)
 
-	resp, err := zClient.locationmanagement.GetLocations()
-	if err != nil {
-		return nil
+	var resp *locationmanagement.Locations
+	id, ok := d.Get("id").(string)
+	if ok && id != "" {
+		log.Printf("[INFO] Getting location information %s\n", id)
+		res, _, err := zClient.locationmanagement.GetLocations()(id)
+		if err != nil {
+			return err
+		}
+		resp = res
 	}
+	name, ok := d.Get("name").(string)
+	if id == "" && ok && name != "" {
+		log.Printf("[INFO] Getting data for server group name %s\n", name)
+		res, _, err := zClient.locationmanagement.GetByName(name)
+		if err != nil {
+			return err
+		}
+		resp = res
+	}
+	if resp != nil {
 
-	d.SetId(resp.ID)
-	_ = d.Set("name", resp.Name)
-	_ = d.Set("parent_id", resp.ParentID)
-	_ = d.Set("up_bandwidth", resp.UpBandwidth)
-	_ = d.Set("dn_bandwidth", resp.DnBandwidth)
-	_ = d.Set("country", resp.Country)
-	_ = d.Set("tz", resp.TZ)
-	_ = d.Set("ip_addresses", resp.IPAddresses)
-	_ = d.Set("ports", resp.Ports)
-	_ = d.Set("auth_required", resp.AuthRequired)
-	_ = d.Set("ssl_scan_enabled", resp.SSLScanEnabled)
-	_ = d.Set("zapp_ssl_scanEnabled", resp.ZappSSLScanEnabled)
-	_ = d.Set("xff_forward_enabled", resp.XFFForwardEnabled)
-	_ = d.Set("surrogate_ip", resp.SurrogateIP)
-	_ = d.Set("idle_time_in_minutes", resp.IdleTimeInMinutes)
-	_ = d.Set("display_time_unit", resp.DisplayTimeUnit)
-	_ = d.Set("surrogate_ip_enforced_for_known_browsers", resp.SurrogateIPEnforcedForKnownBrowsers)
-	_ = d.Set("surrogate_refresh_time_in_minutes", resp.SurrogateRefreshTimeInMinutes)
-	_ = d.Set("surrogate_refresh_time_unit", resp.SurrogateRefreshTimeUnit)
-	_ = d.Set("ofw_enabled", resp.OFWEnabled)
-	_ = d.Set("ips_control", resp.IPSControl)
-	_ = d.Set("aup_enabled", resp.AUPEnabled)
-	_ = d.Set("caution_enabled", resp.CautionEnabled)
-	_ = d.Set("aup_block_internet_until_accepted", resp.AUPBlockInternetUntilAccepted)
-	_ = d.Set("aup_force_ssl_inspection", resp.AUPForceSSLInspection)
-	_ = d.Set("aup_timeout_in_days", resp.AUPTimeoutInDays)
-	_ = d.Set("profile", resp.Profile)
-	_ = d.Set("description", resp.Description)
+		d.SetId(resp.ID)
+		_ = d.Set("name", resp.Name)
+		_ = d.Set("parent_id", resp.ParentID)
+		_ = d.Set("up_bandwidth", resp.UpBandwidth)
+		_ = d.Set("dn_bandwidth", resp.DnBandwidth)
+		_ = d.Set("country", resp.Country)
+		_ = d.Set("tz", resp.TZ)
+		_ = d.Set("ip_addresses", resp.IPAddresses)
+		_ = d.Set("ports", resp.Ports)
+		_ = d.Set("auth_required", resp.AuthRequired)
+		_ = d.Set("ssl_scan_enabled", resp.SSLScanEnabled)
+		_ = d.Set("zapp_ssl_scanEnabled", resp.ZappSSLScanEnabled)
+		_ = d.Set("xff_forward_enabled", resp.XFFForwardEnabled)
+		_ = d.Set("surrogate_ip", resp.SurrogateIP)
+		_ = d.Set("idle_time_in_minutes", resp.IdleTimeInMinutes)
+		_ = d.Set("display_time_unit", resp.DisplayTimeUnit)
+		_ = d.Set("surrogate_ip_enforced_for_known_browsers", resp.SurrogateIPEnforcedForKnownBrowsers)
+		_ = d.Set("surrogate_refresh_time_in_minutes", resp.SurrogateRefreshTimeInMinutes)
+		_ = d.Set("surrogate_refresh_time_unit", resp.SurrogateRefreshTimeUnit)
+		_ = d.Set("ofw_enabled", resp.OFWEnabled)
+		_ = d.Set("ips_control", resp.IPSControl)
+		_ = d.Set("aup_enabled", resp.AUPEnabled)
+		_ = d.Set("caution_enabled", resp.CautionEnabled)
+		_ = d.Set("aup_block_internet_until_accepted", resp.AUPBlockInternetUntilAccepted)
+		_ = d.Set("aup_force_ssl_inspection", resp.AUPForceSSLInspection)
+		_ = d.Set("aup_timeout_in_days", resp.AUPTimeoutInDays)
+		_ = d.Set("profile", resp.Profile)
+		_ = d.Set("description", resp.Description)
+	}
 	return nil
 }
 
