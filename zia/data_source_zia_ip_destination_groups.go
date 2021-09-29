7f587bca604689763a9a8a909a7e628d84f5dc09
William Guilherme <wguilherme@securitygeek.io>
2021-09-29 14:43:04 -0700

    updated ip destination groups data source

--- zia/data_source_zia_ip_destination_groups.go
+++ zia/data_source_zia_ip_destination_groups.go
@@ -38,7 +38,7 @@ func dataSourceIPDestinationGroupsRead(d *schema.ResourceData, m interface{}) er
 	idObj, idSet := d.GetOk("id")
 	id, idIsInt := idObj.(int)
 	if idSet && idIsInt && id > 0 {
-		log.Printf("[INFO] Getting data for user id: %d\n", id)
+		log.Printf("[INFO] Getting data for ip destination groups id: %d\n", id)
 		res, err := zClient.ipdestinationgroups.GetIPDestinationGroupsLite(id)
 		if err != nil {
 			return err
@@ -47,7 +47,7 @@ func dataSourceIPDestinationGroupsRead(d *schema.ResourceData, m interface{}) er
 	}
 	name, _ := d.Get("name").(string)
 	if resp == nil && name != "" {
-		log.Printf("[INFO] Getting data for user : %s\n", name)
+		log.Printf("[INFO] Getting data for ip destination groups : %s\n", name)
 		res, err := zClient.ipdestinationgroups.GetIPDestinationGroupsLiteByName(name)
 		if err != nil {
 			return err
@@ -61,7 +61,7 @@ func dataSourceIPDestinationGroupsRead(d *schema.ResourceData, m interface{}) er
 		_ = d.Set("extensions", resp.Extensions)
 
 	} else {
-		return fmt.Errorf("couldn't find any user with name '%s' or id '%d'", name, id)
+		return fmt.Errorf("couldn't find any ip destination groups with name '%s' or id '%d'", name, id)
 	}
 
 	return nil
