23608c1a360921cc74bbff017cec03fc04957eb8
William Guilherme <wguilherme@securitygeek.io>
2021-10-06 02:28:40 -0400

    updated zia_activation example

--- examples/zia_activation/main.tf
+++ examples/zia_activation/main.tf
@@ -9,19 +9,21 @@ terraform {
 
 provider "zia" {}
 
-/*
-data "zia_activation_status" "example"{
-}
 
-output "zia_activation_status"{
-    value = data.zia_activation_status.example
-}
-*/
-
-resource "zia_activation_status" "example"{
+resource "zia_activation_status" "example1"{
     status = "ACTIVE"
 }
 
-output "zia_activation_status"{
-    value = zia_activation_status.example
+output "zia_activation_status_example1"{
+    value = zia_activation_status.example1
 }
+
+data "zia_activation_status" "example2"{
+}
+
+output "zia_activation_status_example2"{
+    value = data.zia_activation_status.example2
+}
+
+
+
