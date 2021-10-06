bd01dd750c7256fe15b3444377a35e222153f10a
William Guilherme <wguilherme@securitygeek.io>
2021-10-05 22:31:16 -0400

    updated location management example

--- examples/zia_location_management/main.tf
+++ examples/zia_location_management/main.tf
@@ -12,7 +12,7 @@ provider "zia" {}
 resource "zia_location_management" "toronto"{
     name = "SGIO-IPSEC-Toronto"
     description = "Created with Terraform"
-    //ip_addresses = [ zia_traffic_forwarding_static_ip.example.static_ip_id ]
+    ip_addresses = [ zia_traffic_forwarding_static_ip.example.static_ip_id ]
     vpn_credentials {
        id = zia_traffic_forwarding_vpn_credentials.example.vpn_credental_id
        type = zia_traffic_forwarding_vpn_credentials.example.type
@@ -32,41 +32,12 @@ output "zia_location_management"{
     // sensitive = true
 }
 
-/*
+
 resource "zia_traffic_forwarding_static_ip" "example"{
     ip_address =  "50.98.112.169"
     routable_ip = true
     comment = "Created with Terraform"
     geo_override = false
 }
-
-
-
-
-*/
-
-
-/*
-data "zia_activation_status" "example"{
-}
-
-output "zia_activation_status"{
-    value = data.zia_activation_status.example
-}
-
-data "zia_location_management" "vancouver"{
-    name = "SGIO-IPSEC-Vancouver"
-}
-
-output "zia_location_management"{
-    value = data.zia_location_management.vancouver
-}
-
-
-
-
-
-
-*/
 
 
