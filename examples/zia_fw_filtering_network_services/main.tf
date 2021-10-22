cb939d8c978393df6c2e639422612691d73b89f3
William Guilherme <wguilherme@securitygeek.io>
2021-10-22 14:48:54 -0700

    updated zia_fw_filtering_network_services example

--- examples/zia_fw_filtering_network_services/main.tf
+++ examples/zia_fw_filtering_network_services/main.tf
@@ -13,34 +13,53 @@ resource "zia_firewall_filtering_network_service" "example" {
   name        = "example"
   description = "example"
   src_tcp_ports {
-    start = 123
-    end   = 125
+    start = 5000
+  }
+  src_tcp_ports {
+    start = 5001
+  }
+  src_tcp_ports {
+    start = 5002
+  }
+  src_tcp_ports {
+    start = 5003
+    end = 5005
   }
   dest_tcp_ports {
-    start = 123
-    end   = 125
+    start = 5000
   }
-  src_udp_ports {
-    start = 123
-    end   = 125
+    dest_tcp_ports {
+    start = 5001
   }
-  src_udp_ports {
-    start = 126
-    end   = 127
+  dest_tcp_ports {
+    start = 5002
   }
+    dest_tcp_ports {
+    start = 5003
+    end = 5005
+  }
+  // src_udp_ports {
+    // start = 123
+    // end   = 125
+  // }
+  // src_udp_ports {
+    // start = 126
+    // end   = 127
+  // }
 
-  dest_udp_ports {
-    start = 123
-    end   = 125
-  }
-  dest_udp_ports {
-    start = 126
-    end   = 127
-  }
+  // dest_udp_ports {
+    // start = 123
+    // end   = 125
+  // }
+  // dest_udp_ports {
+    // start = 126
+    // end   = 127
+  // }
   type = "CUSTOM"
 }
 
 
+/*
 data "zia_firewall_filtering_network_service" "example" {
   name = zia_firewall_filtering_network_service.example.name
 }
@@ -48,3 +67,4 @@ data "zia_firewall_filtering_network_service" "example" {
 output "zia_firewall_filtering_network_service" {
   value = data.zia_firewall_filtering_network_service.example
 }
+*/
