493c708989696482e023665c61d8490348e83b8a
William Guilherme <wguilherme@securitygeek.io>
2021-10-25 16:25:07 -0700

    Added zia_firewall_filtering_rule example

---
+++ examples/zia_firewall_filtering_rule/basic.tf
@@ -0,0 +1,38 @@
+resource "zia_firewall_filtering_rule" "example" {
+    name = "Example"
+    description = "Example"
+    action = "ALLOW"
+    state = "ENABLED"
+    order = 1
+    enable_full_logging = true
+    nw_services {
+        id = [ data.zia_firewall_filtering_network_service.zscaler_proxy_nw_services.id ]
+    }
+    departments {
+        id = [ data.zia_department_management.engineering.id ]
+    }
+    groups {
+        id = [ data.zia_group_management.normal_internet.id ]
+    }
+    time_windows {
+        id = [ data.zia_firewall_filtering_time_window.work_hours.id ]
+    }
+}
+
+data "zia_firewall_filtering_network_service" "zscaler_proxy_nw_services" {
+    name = "ZSCALER_PROXY_NW_SERVICES"
+}
+
+data "zia_department_management" "engineering" {
+ name = "Engineering"
+}
+
+data "zia_group_management" "normal_internet" {
+    name = "Normal_Internet"
+}
+
+data "zia_firewall_filtering_time_window" "work_hours" {
+    name = "Work hours"
+}
+
+
