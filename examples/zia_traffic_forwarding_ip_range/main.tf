7987643de4018034fe0520290da1d80c503b5a31
William Guilherme <wguilherme@securitygeek.io>
2021-10-27 11:03:27 -0700

    updated zia_traffic_forwarding_ip_range example

--- examples/zia_traffic_forwarding_ip_range/main.tf
+++ examples/zia_traffic_forwarding_ip_range/main.tf
@@ -1,14 +1,13 @@
-terraform {
-    required_providers {
-        zia = {
-            version = "1.0.0"
-            source = "zscaler.com/zia/zia"
-        }
-    }
+// It will return the first 10 available gre internal ip ranges
+data "zia_gre_internal_ip_range_list" "example"{
 }
 
-provider "zia" {}
+output "zia_gre_internal_ip_range_list_example"{
+    value = data.zia_gre_internal_ip_range_list.example
+}
 
+// Explicitly retrieves the first 10 available gre internal ip ranges
+// You can select any number in the required_count
 data "zia_gre_internal_ip_range_list" "example"{
     required_count = 10
 }
