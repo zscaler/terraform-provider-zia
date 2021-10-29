5cd1f78ade39148a90f0af18c4b85ee2e7d4fef5
William Guilherme <wguilherme@securitygeek.io>
2021-10-29 09:23:35 -0700

    updated zia_department_management

--- examples/user_management/zia_department_management/datasource.tf
+++ examples/user_management/zia_department_management/datasource.tf
@@ -2,7 +2,7 @@ data "zia_department_management" "engineering" {
  name = "Engineering"
 }
 
-output "zia_department_management" {
+output "zia_department_management_engineering" {
   value = data.zia_department_management.engineering
 }
 
@@ -10,6 +10,6 @@ data "zia_department_management" "finance" {
  name = "Finance"
 }
 
-output "zia_department_management" {
+output "zia_department_management_finance" {
   value = data.zia_department_management.finance
 }
