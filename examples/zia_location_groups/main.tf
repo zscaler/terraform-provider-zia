455092ba4ee82cdba93c1b60a5028cecafb8d35b
William Guilherme <wguilherme@securitygeek.io>
2021-10-28 20:49:42 -0700

    updated location_groups example

--- examples/zia_location_groups/main.tf
+++ examples/zia_location_groups/main.tf
@@ -1,8 +1,31 @@
-data "zia_location_groups" "example"{
+data "zia_location_groups" "example1"{
     name = "Corporate User Traffic Group"
 }
 
-output "zia_location_groups"{
-    value = data.zia_location_groups.example
+output "zia_location_groups_corporate"{
+    value = data.zia_location_groups.example1
 }
 
+data "zia_location_groups" "example2"{
+    name = "Guest Wifi Group"
+}
+
+output "zia_location_groups_wifi"{
+    value = data.zia_location_groups.example2
+}
+
+data "zia_location_groups" "example3"{
+    name = "IoT Traffic Group"
+}
+
+output "zia_location_groups_iot"{
+    value = data.zia_location_groups.example3
+}
+
+data "zia_location_groups" "example4"{
+    name = "Server Traffic Group"
+}
+
+output "zia_location_groups_server"{
+    value = data.zia_location_groups.example4
+}
