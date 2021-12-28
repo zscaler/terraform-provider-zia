3511018428dbbeb0332dfe9b48757d7d4996922f
William Guilherme <wguilherme@securitygeek.io>
2021-12-28 15:36:49 -0800

    updated main.go

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"log"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/willguibr/terraform-provider-zia/zia"
 )
 
 func main() {
-	var debugMode bool
-
-	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-
-	if debugMode {
-		err := plugin.Debug(context.Background(), "github.com/willguibr/terraform-provider-zia",
-			&plugin.ServeOpts{
-				ProviderFunc: zia.Provider,
-			})
-		if err != nil {
-			log.Println(err.Error())
-		}
-	} else {
-		plugin.Serve(&plugin.ServeOpts{
-			ProviderFunc: zia.Provider})
-	}
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: zia.Provider,
+	})
 }
