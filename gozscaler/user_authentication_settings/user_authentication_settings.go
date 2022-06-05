6b15ea73dcd71b70d9546d51445981dd58745e74
amazzalel-habib <amazzal.elhabib@gmail.com>
2022-06-05 22:14:16 +0100

    add data & resource for auth settings exempted urls

--- gozscaler/user_authentication_settings/user_authentication_settings.go
+++ gozscaler/user_authentication_settings/user_authentication_settings.go
@@ -1,5 +1,10 @@
 package user_authentication_settings
 
+import (
+	"fmt"
+	"log"
+)
+
 const (
 	exemptedUrlsEndpoint = "/authSettings/exemptedUrls"
 )
@@ -10,5 +15,56 @@ var AddRemoveURLFromList []string = []string{
 }
 
 type ExemptedUrls struct {
-	URLs map[string]interface{} `json:"urls"`
+	URLs []string `json:"urls"`
+}
+
+func (service *Service) Get() (*ExemptedUrls, error) {
+	var urls ExemptedUrls
+	err := service.Client.Read(exemptedUrlsEndpoint, &urls)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Returning exempted url rules from Get: %v", urls)
+	return &urls, nil
+}
+
+// return the new items that were added to slice1
+func difference(slice1 []string, slice2 []string) []string {
+	var diff []string
+	for _, s1 := range slice1 {
+		found := false
+		for _, s2 := range slice2 {
+			if s1 == s2 {
+				found = true
+				break
+			}
+		}
+		if !found {
+			diff = append(diff, s1)
+		}
+	}
+	return diff
+}
+
+func (service *Service) Update(urls ExemptedUrls) (*ExemptedUrls, error) {
+	currentUrsl, err := service.Get()
+	if err != nil {
+		return nil, err
+	}
+	newUrls := difference(urls.URLs, currentUrsl.URLs)
+	removedUrls := difference(currentUrsl.URLs, urls.URLs)
+	if len(newUrls) > 0 {
+		_, err := service.Client.Create(fmt.Sprintf("%s?action=ADD_TO_LIST", exemptedUrlsEndpoint), ExemptedUrls{newUrls})
+		if err != nil {
+			return nil, err
+		}
+	}
+	if len(removedUrls) > 0 {
+		_, err := service.Client.Create(fmt.Sprintf("%s?action=REMOVE_FROM_LIST", exemptedUrlsEndpoint), ExemptedUrls{removedUrls})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &urls, nil
 }
--- zia/provider.go
+++ zia/provider.go
@@ -64,6 +64,7 @@ func Provider() *schema.Provider {
 			"zia_user_management":                               resourceUserManagement(),
 			"zia_activation_status":                             resourceActivationStatus(),
 			"zia_rule_labels":                                   resourceRuleLabels(),
+			"zia_auth_settings_urls":                            resourceAuthSettingsUrls(),
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{
@@ -99,6 +100,7 @@ func Provider() *schema.Provider {
 			"zia_devices":                                       dataSourceDevices(),
 			"zia_rule_labels":                                   dataSourceRuleLabels(),
 			"zia_activation_status":                             dataSourceActivationStatus(),
+			"zia_auth_settings_urls":                            dataSourceAuthSettingsUrls(),
 		},
 
 		ConfigureFunc: ziaConfigure,
