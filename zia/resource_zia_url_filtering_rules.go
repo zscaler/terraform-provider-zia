24c916a65ef8d4ee2229213aeaa70331326aa99f
GitHub Terraform <zscaler-github-P@zscaler.com>
2023-02-09 22:39:23 -0800

    deployed ordering fix to zia_url_filtering

--- zia/resource_zia_url_filtering_rules.go
+++ zia/resource_zia_url_filtering_rules.go
@@ -1,11 +1,16 @@
 package zia
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 	"sync"
+	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	client "github.com/zscaler/zscaler-sdk-go/zia"
@@ -27,6 +32,10 @@ func resourceURLFilteringRules() *schema.Resource {
 		Read:   resourceURLFilteringRulesRead,
 		Update: resourceURLFilteringRulesUpdate,
 		Delete: resourceURLFilteringRulesDelete,
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Update: schema.DefaultTimeout(20 * time.Minute),
+		},
 		Importer: &schema.ResourceImporter{
 			State: func(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
 				zClient := m.(*Client)
@@ -179,24 +188,26 @@ func resourceURLFilteringRulesCreate(d *schema.ResourceData, m interface{}) erro
 
 	req := expandURLFilteringRules(d)
 	log.Printf("[INFO] Creating url filtering rule\n%+v\n", req)
-	orderObj, orderIsSet := d.GetOk("order")
-	if orderIsSet {
-		// always set it to 1, and let the re-ordering happen after ( because having an invalid order will cause a bad request)
-		req.Order = 1
-	}
-	resp, err := zClient.urlfilteringpolicies.Create(&req)
-	if err != nil {
-		return err
-	}
-	if orderIsSet {
-		req.Order = orderObj.(int)
-		go reorder(req.Order, resp.ID, zClient)
-	}
-	log.Printf("[INFO] Created zia url filtering rule request. ID: %v\n", resp)
-	d.SetId(strconv.Itoa(resp.ID))
-	_ = d.Set("rule_id", resp.ID)
+	return resource.RetryContext(context.Background(), d.Timeout(schema.TimeoutCreate)-time.Minute, func() *resource.RetryError {
+		resp, err := zClient.urlfilteringpolicies.Create(&req)
+		if err != nil {
+			if strings.Contains(err.Error(), "INVALID_INPUT_ARGUMENT") {
+				time.Sleep(time.Second * time.Duration(req.Order+1))
+				return resource.RetryableError(errors.New("expected resource to be created but was not"))
+			}
+			return resource.NonRetryableError(fmt.Errorf("error creating resource: %s", err))
+		}
+		log.Printf("[INFO] Created url firewall filtering rule request. ID: %v\n", resp)
+		d.SetId(strconv.Itoa(resp.ID))
+		_ = d.Set("rule_id", resp.ID)
 
-	return resourceURLFilteringRulesRead(d, m)
+		err = resourceURLFilteringRulesRead(d, m)
+		if err != nil {
+			return resource.NonRetryableError(err)
+		} else {
+			return nil
+		}
+	})
 }
 
 func resourceURLFilteringRulesRead(d *schema.ResourceData, m interface{}) error {
@@ -300,24 +311,29 @@ func resourceURLFilteringRulesUpdate(d *schema.ResourceData, m interface{}) erro
 	}
 	log.Printf("[INFO] Updating url filtering rule ID: %v\n", id)
 	req := expandURLFilteringRules(d)
-	if d.HasChange("order") {
-		_, orderIsSet := d.GetOk("order")
-		if orderIsSet {
-			go reorder(req.Order, req.ID, zClient)
-		}
-		req.Order = 1
-	}
 	if _, err := zClient.urlfilteringpolicies.Get(id); err != nil {
 		if respErr, ok := err.(*client.ErrorResponse); ok && respErr.IsObjectNotFound() {
 			d.SetId("")
 			return nil
 		}
 	}
-	if _, _, err := zClient.urlfilteringpolicies.Update(id, &req); err != nil {
-		return err
-	}
+	return resource.RetryContext(context.Background(), d.Timeout(schema.TimeoutUpdate)-time.Minute, func() *resource.RetryError {
+		_, _, err := zClient.urlfilteringpolicies.Update(id, &req)
+		if err != nil {
+			if strings.Contains(err.Error(), "INVALID_INPUT_ARGUMENT") {
+				time.Sleep(time.Second * time.Duration(req.Order+1))
+				return resource.RetryableError(errors.New("expected resource to be updated but was not"))
+			}
+			return resource.NonRetryableError(fmt.Errorf("error updating resource: %s", err))
+		}
 
-	return resourceURLFilteringRulesRead(d, m)
+		err = resourceURLFilteringRulesRead(d, m)
+		if err != nil {
+			return resource.NonRetryableError(err)
+		} else {
+			return nil
+		}
+	})
 }
 
 func resourceURLFilteringRulesDelete(d *schema.ResourceData, m interface{}) error {
