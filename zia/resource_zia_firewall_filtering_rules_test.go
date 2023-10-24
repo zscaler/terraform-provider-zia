6b277dc353524e10969606958ef814824500bd99
William Guilherme <wguilherme@securitygeek.io>
2023-10-24 16:39:01 -0700

    fix: Fix firewall filtering rule test order

--- zia/resource_zia_firewall_filtering_rules_test.go
+++ zia/resource_zia_firewall_filtering_rules_test.go
@@ -228,7 +228,7 @@ resource "%s" "%s" {
 	description = "%s"
 	action = "%s"
 	state = "%s"
-	order = 4
+	order = 1
 	enable_full_logging = "%s"
 	nw_services {
 		id = [ data.zia_firewall_filtering_network_service.zscaler_proxy_nw_services.id ]
