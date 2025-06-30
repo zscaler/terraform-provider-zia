5e9c0601bed4b2e2253be43e7c27b06379b84a18
William Guilherme <wguilherme@securitygeek.io>
2025-06-30 11:49:00 -0700

    fix: Fixed zia_file_type_control_rules url_categories attribute

--- zia/resource_zia_file_type_control_rules.go
+++ zia/resource_zia_file_type_control_rules.go
@@ -339,7 +339,11 @@ func resourceFileTypeControlRulesRead(ctx context.Context, d *schema.ResourceDat
 	_ = d.Set("protocols", resp.Protocols)
 	_ = d.Set("file_types", resp.FileTypes)
 	_ = d.Set("cloud_applications", resp.CloudApplications)
-	_ = d.Set("url_categories", resp.URLCategories)
+	if len(resp.URLCategories) == 0 {
+		_ = d.Set("url_categories", []string{"ANY"})
+	} else {
+		_ = d.Set("url_categories", resp.URLCategories)
+	}
 	_ = d.Set("device_trust_levels", resp.DeviceTrustLevels)
 	_ = d.Set("max_size", resp.MaxSize)
 	_ = d.Set("min_size", resp.MinSize)
